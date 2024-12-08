97c596ec56ac2cccafce1099017638a56c9ad9c5
DenVik <denyagarkusha2@gmail.com>
2024-12-08 19:10:55 +0100

    js settings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,31 +4,17 @@ import Heroes from "../pages/Heroes/Heroes";
 import Read from "../pages/Read/Read.jsx";
 import NotFound from "../pages/NotFound/NotFound";
 import Home from "../pages/Home/Home.jsx";
-import ReadDetails from "../pages/ReadDetails/ReadDetails.jsx";
-import ReadChapter from "../components/ReadChapter/ReadChapter.jsx";
-
+import ReadChapter from '../components/ReadChapter/ReadChapter';
 const App = () => {
   return (
     <div>
-      {/* <Routes>
-        <Route path="/" element={<Header />}>
-          <Route index element={<Home />} />
-          <Route path="heroes" element={<Heroes />} />
-          <Route path="read" element={<Read />} />
-          <Route path="read/:readId" element={<ReadDetails />}>
-            <Route path="chapter" element={<ReadChapter />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes> */}
-
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/heroes" element={<Heroes />} />
         <Route path="/read" element={<Read />} />
 
-        <Route path="/read/:readId" element={<ReadDetails />} />
+        <Route path="/read/:readId" element={<ReadChapter/>} />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
--- src/pages/Read/Read.jsx
+++ src/pages/Read/Read.jsx
@@ -1,11 +1,15 @@
+import ChapterButtons from "../../components/ChapterButtons/ChapterButtons";
 import Footer from "../../components/Footer/Footer";
 import StarryBackground from "../../components/StarryBackground/StarryBackground";
+import read from '../../assets/data/read.json'
 
 const Read = () => {
   return (
     <div style={{ position: "relative", minHeight: "100vh" }}>
       <StarryBackground />
       <div style={{ position: "relative", zIndex: 1 }}>
+        <h1>Незнайка на луне</h1>
+        <ChapterButtons readData={read}/>
         <Footer />
       </div>
     </div>
